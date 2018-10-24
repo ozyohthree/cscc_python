f4744334cb7b306055be493d037bf83865c09143
Ohthree <ozyohthree@gmail.com>
2018-10-24 08:29:01 -0400

    updates

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,8 @@
 
 def fizz_buzz(n):
     for x in range(1,n+1):
-        if (x%3 == 0 and x%5 == 0):
+        #if (x%3 == 0 and x%5 == 0):
+        if (x%(3*5)==0):
             print ("fizzbuzz")
         elif (x%3==0):
             print ('fizz')
